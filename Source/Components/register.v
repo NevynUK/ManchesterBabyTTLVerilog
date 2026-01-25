7b9f4f70eb148cc2d0b4155bc0a9c3b0fa114798
Mark Stevens <mark@thepcsite.co.uk>
2026-01-25 13:10:34 +0000

    Add output to ALU.

--- Source/Components/register.v
+++ Source/Components/register.v
@@ -13,7 +13,8 @@ module register
     input wire [31:0] A,        // 32-bit data input.
     input wire LE,              // Latch Enable (active high): loads data on positive edge.
     input wire OE_n,            // Output Enable (active low): outputs Q when low, High-Z when high.
-    output wire [31:0] Q        // 32-bit data output.
+    output wire [31:0] Q,       // 32-bit data output.
+    output wire [31:0] to_alu   // 32-bit data output to ALU (always active).
 );
     //
     //  32-bit register using four 74LS373 8-bit latches.
@@ -22,5 +23,13 @@ module register
     ttl373_latch latch1 (.D(A[15:8]),  .LE(LE), .OE_n(OE_n), .Q(Q[15:8]));
     ttl373_latch latch2 (.D(A[23:16]), .LE(LE), .OE_n(OE_n), .Q(Q[23:16]));
     ttl373_latch latch3 (.D(A[31:24]), .LE(LE), .OE_n(OE_n), .Q(Q[31:24]));
+
+    //
+    //  Always-active output to ALU (bypasses OE_n control).
+    //
+    ttl373_latch latch0_alu (.D(A[7:0]),   .LE(LE), .OE_n(1'b0), .Q(to_alu[7:0]));
+    ttl373_latch latch1_alu (.D(A[15:8]),  .LE(LE), .OE_n(1'b0), .Q(to_alu[15:8]));
+    ttl373_latch latch2_alu (.D(A[23:16]), .LE(LE), .OE_n(1'b0), .Q(to_alu[23:16]));
+    ttl373_latch latch3_alu (.D(A[31:24]), .LE(LE), .OE_n(1'b0), .Q(to_alu[31:24]));
 
 endmodule
